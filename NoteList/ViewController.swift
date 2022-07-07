2cfb2f25317e3b8a2ece5edd5bafce6f9b5e286e
maximsamus <maximsamus1@gmail.com>
2022-07-07 12:33:20 +0300

    Initial Commit

---
+++ NoteList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteList
+//
+//  Created by Максим Самусь on 07.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
